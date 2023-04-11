14dbf94f7c3acffae1bc95246fa6dd66f7fd7e72
Taysa86 <Taysarolu86@gmail.com>
2023-04-11 00:11:39 -0500

    added starter code to package.json removed duplicate code in routes

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -4,9 +4,3 @@ module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   );
-  const path = require('path');
-
-  module.exports = (app) =>
-    app.get('/', (req, res) =>
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-    );
